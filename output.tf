d34819b5cfe81d03fdb13e59ff1365d2fe69f262
Enes Turan <ahmedenesturan@gmail.com>
2023-05-28 15:42:16 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,17 @@
-output "ssh_command_bastion_host" {
+output "ssh_command_minikube" {
   value = "To SSH into the bastion host, use the following gcloud command:\n\n  gcloud compute ssh ${google_compute_instance.minikube.name} --zone ${var.zone} --project ${var.project_id} --tunnel-through-iap\n\nMake sure you have the gcloud command-line tool installed and configured on your local machine before running the command. Once you have SSHed into the bastion host, you can then use it to access other resources in your Google Cloud environment."
 }
 
 
-output "instance_details_bastion" {
+output "instance_details_minikube" {
   value = {
-    name        = google_compute_instance.minikube.name
-    internal_ip = google_compute_instance.minikube.network_interface[0].network_ip
-    instance_id = google_compute_instance.minikube.id
-    console     = "https://console.cloud.google.com/compute/instancesDetail/zones/${var.zone}/instances/${google_compute_instance.minikube.name}"
+    instance_name    = google_compute_instance.minikube.name
+    instance_id      = google_compute_instance.minikube.id
+    instance_zone    = google_compute_instance.minikube.zone
+    instance_network = google_compute_instance.minikube.network_interface.0.network
+    instance_labels  = google_compute_instance.minikube.labels
+    ip_internal      = google_compute_instance.minikube.network_interface[0].network_ip
+    ip_external      = google_compute_instance.minikube.network_interface.0.access_config.0.nat_ip
+    console          = "https://console.cloud.google.com/compute/instancesDetail/zones/${var.zone}/instances/${google_compute_instance.minikube.name}"
   }
 }
