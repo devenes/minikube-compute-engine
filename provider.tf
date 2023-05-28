28e6733fcb4a594b11730a332b861cf1ef4ea675
Enes Turan <ahmedenesturan@gmail.com>
2023-05-28 03:06:08 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,10 +8,10 @@ provider "google" {
 # Configure the Terraform backend to store the state in a Google Cloud Storage bucket
 # https://www.terraform.io/language/settings/backends/gcs
 terraform {
-  backend "gcs" {
-    bucket = "your-bucket-name"
-    prefix = "terraform/state"
-  }
+  # backend "gcs" {
+  #   bucket = "your-bucket-name"
+  #   prefix = "terraform/state"
+  # }
 
   # Require the Google provider
   required_providers {
